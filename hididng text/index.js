e2b76a17fe478c9ba022ca89b4e54d19a32716f4
Dhruv <samantdhruv@gmail.com>
2021-09-27 13:27:10 +0530

    text hiding

---
+++ hididng text/index.js
@@ -0,0 +1,9 @@
+
+const text = document.querySelector("#text");
+function toggleText(){
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
