24ad85cacf69d8c5a38a3b8d1fc39ed09c6e8268
Dhruv <samantdhruv@gmail.com>
2021-09-28 12:15:49 +0530

    highest marks

---
+++ 6. highest marks/index.js
@@ -0,0 +1,18 @@
+const readlineSync = require ('readline-sync');
+var highestMarks= 0;
+
+const name = readlineSync.question("Enter your name! ");
+const unitTest = readlineSync.question("Enter your unit test marks! ");
+const preFinal = readlineSync.question("Enter your pre final marks! ");
+const final = readlineSync.question("Enter your final marks! ");
+const totalMarks = Number(unitTest)+Number(preFinal)+Number(final);
+console.log(totalMarks);
+
+if(totalMarks>highestMarks){
+  var topperName = name;
+  highestMarks = totalMarks;
+}
+const average = Number(unitTest)+Number(preFinal)+Number(final)/300;
+console.log("The average marks are: "+average); 
+
+
