f1948aac7cd9eba4a002904cec287acc11affa3c
Dhruv <samantdhruv@gmail.com>
2021-09-28 10:57:06 +0530

    change in logic of size +

--- 3. size +/index.js
+++ 3. size +/index.js
@@ -1,15 +1,36 @@
-const text = document.querySelector("#text");
+// const text = document.querySelector("#text");
+const inputText = document.querySelector("#input-text");
 const bigBtn = document.querySelector("#big-btn");
 const smallBtn = document.querySelector("#small-btn");
+const outputDiv = document.querySelector("#output");
 
 var number = 32;
 
-bigBtn.addEventListener('click', ()=>{
-    number += 2;
-    text.style.fontSize= `${number}px`;  
-});
+function increace (text) {
+    var plusTwo= document.createElement('plusTwo');
+    plusTwo.appendChild(document.createTextNode(text));
+      document.body.appendChild(plusTwo);
 
-smallBtn.addEventListener('click', ()=>{
+    number += 2;
+    plusTwo.style.fontSize= `${number}px `;  
+};
+
+function toIncreace () {
+    outputDiv.text = increace(inputText.value);
+}
+bigBtn.addEventListener("click", toIncreace)
+
+
+function decrease (text) {
+    var minusTwo= document.createElement('minusTwo');
+    minusTwo.appendChild(document.createTextNode(text));
+      document.body.appendChild(minusTwo);
+
     number -= 2;
-    text.style.fontSize= `${number}px`
-});
+    minusTwo.style.fontSize= `${number}px `;  
+};
+
+function toDecrease () {
+    outputDiv.text = decrease(inputText.value);
+}
+smallBtn.addEventListener("click", toDecrease)
