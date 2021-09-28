9b9bdfa57f2579ffdbb1b8aed932477278794192
Dhruv <samantdhruv@gmail.com>
2021-09-28 20:47:21 +0530

    logic change size changer

--- 4. size changer/index.js
+++ 4. size changer/index.js
@@ -1,39 +1,71 @@
-const inputText = document.querySelector("#input-text");
-const h1Btn = document.querySelector("#h1-btn");
-const h2Btn = document.querySelector("#h2-btn");
-const h3Btn = document.querySelector("#h3-btn");
-const outputDiv = document.querySelector("#output");
+var input = document.querySelector(".input")
+var btnOne = document.querySelector("#button1")
+var btnTwo = document.querySelector("#button2")
+var btnThree = document.querySelector("#button3")
+var outputEl = document.querySelector("#output")
+
+btnOne.addEventListener('click',function clickHandler1(){
+    var inputData = input.value
+    outputEl.innerHTML = `<h1>${inputData}</h1>`
+
+})
 
 
-function headOne(text) {
-    let h1 = document.createElement("h1");
-    h1.appendChild(document.createTextNode(text));
-    document.body.appendChild(h1);
-}
-function toH1() {
-    outputDiv.text = headOne(inputText.value);
-}
-h1Btn.addEventListener("click", toH1);
+btnTwo.addEventListener('click',function clickHandler1(){
+    var inputData = input.value
+    outputEl.innerHTML = `<h2>${inputData}</h2>`
 
-function headTwo(text) {
-    let h2 = document.createElement("h2");
-    h2.appendChild(document.createTextNode(text));
-    document.body.appendChild(h2);
-}
-function toH2() {
-    outputDiv.text = headTwo(inputText.value);
-}
-h2Btn.addEventListener("click", toH2);
+})
 
-function headThree(text) {
-    let h3 = document.createElement("h3");
-    h3.appendChild(document.createTextNode(text));
-    document.body.appendChild(h3);
-}
-function toH3() {
-    outputDiv.text = headThree(inputText.value);
-}
-h3Btn.addEventListener("click", toH3);
+
+
+btnThree.addEventListener('click',function clickHandler1(){
+    var inputData = input.value
+    outputEl.innerHTML = `<h3>${inputData}</h3>`
+})
+
+
+
+
+
+
+
+// const inputText = document.querySelector("#input-text");
+// const h1Btn = document.querySelector("#h1-btn");
+// const h2Btn = document.querySelector("#h2-btn");
+// const h3Btn = document.querySelector("#h3-btn");
+// const outputDiv = document.querySelector("#output");
+
+
+// function headOne(text) {
+//     let h1 = document.createElement("h1");
+//     h1.appendChild(document.createTextNode(text));
+//     document.body.appendChild(h1);
+// }
+// function toH1() {
+//     outputDiv.text = headOne(inputText.value);
+// }
+// h1Btn.addEventListener("click", toH1);
+
+// function headTwo(text) {
+//     let h2 = document.createElement("h2");
+//     h2.appendChild(document.createTextNode(text));
+//     document.body.appendChild(h2);
+// }
+// function toH2() {
+//     outputDiv.text = headTwo(inputText.value);
+// }
+// h2Btn.addEventListener("click", toH2);
+
+// function headThree(text) {
+//     let h3 = document.createElement("h3");
+//     h3.appendChild(document.createTextNode(text));
+//     document.body.appendChild(h3);
+// }
+// function toH3() {
+//     outputDiv.text = headThree(inputText.value);
+// }
+// h3Btn.addEventListener("click", toH3);
 
 
 
