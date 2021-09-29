7b6c63b933bf8f1678e10c9e17edafcd4c0a0855
Dhruv <samantdhruv@gmail.com>
2021-09-29 11:16:44 +0530

    logic change

--- 6. highest marks/index.js
+++ 6. highest marks/index.js
@@ -1,41 +1,20 @@
+var readlineSync = require ('readline-sync');
+var highestMarks= 0;
 
+for (let i=0; i < 5; i++) {
+var name = readlineSync.question("Enter your name! ");
+var unitTest = readlineSync.question("Enter your unit test marks! ");
+var preFinal = readlineSync.question("Enter your pre final marks! ");
+var final = readlineSync.question("Enter your final marks! ");
 
-var student1 = {
-  name: "a",
-  unitTestMarks: 20,
-  preFinalMarks: 50,
-  finalMarks: 80,
-  totalMarks: 150
-}
-var student2 = {
-  name: "b",
-  unitTestMarks: 19,
-  preFinalMarks: 40,
-  finalMarks: 90,
-  totalMarks: 149
-}
-var student3 = {
-  name: "c",
-  unitTestMarks: 18,
-  preFinalMarks: 60,
-  finalMarks: 40,
-  totalMarks: 118
-}
-var student4 = {
-  name: "d",
-  unitTestMarks: 17,
-  preFinalMarks: 30,
-  finalMarks: 60,
-  totalMarks: 107
-}
-var student5 = {
-  name: "e",
-  unitTestMarks: 16,
-  preFinalMarks: 70,
-  finalMarks: 100,
-  totalMarks: 186
+var totalMarks = Number(unitTest)+Number(preFinal)+Number(final);
+console.log(totalMarks);
 }
 
-console.log(student5.totalMarks)
+if(totalMarks>highestMarks){
+  highestMarks = totalMarks;
+}
+var average = Number(unitTest)+Number(preFinal)+Number(final)/300;
+console.log("The average marks are: "+average); 
 
-console.log((student1.totalMarks + student2.totalMarks + student3.totalMarks + student4.totalMarks + student5.totalMarks) / 5)
+
