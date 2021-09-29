0de97ea9b521f1c30c86a2719b74f298fdbc1e07
Dhruv <samantdhruv@gmail.com>
2021-09-29 14:47:57 +0530

    no luck yet

--- 10. 404 401/index.js
+++ 10. 404 401/index.js
@@ -1,21 +1,35 @@
 const error401Btn = document.querySelector("#error1-btn");
 const error404Btn = document.querySelector("#error4-btn");
-const errorMsg = document.querySelector("#error-msg")
+
 
 var serverURL1 = "https://unitube-server.herokuapp.com/playlists"
 var serverURL2 = "https://jsonplaceholder.typicode.com/dummyUsers"
 
-function getURL (text) {
-    return serverURL1 + "?" + "text=" + text
+function getURL1(message) {
+    return serverURL1 + "?" + "text=" + message
 }
 
 error401Btn.addEventListener("click", function error401 () {
-    var inputError = errorMsg.value
-    fetch(getURL(inputError))
+    var inputError = "You Are Not Logged In"
+    fetch(getURL1(inputError))
     .then(response => response.json())
     .then(json => {
-        var errorMsg = json.message;
-        alert(errorMsg)
+        var errorMsg1 = json.message;
+        alert(errorMsg1)
+    })
+        
+})
+
+function getURL2(text) {
+    return serverURL2 + "?" + "text=" + text 
+}
+
+error404Btn.addEventListener("click", function error404 () {
+    var inputError = "Page Not Found"
+    fetch(getURL2(inputError))
+    .then(response => response.json())
+    .then(json => {
+        alert(json)
     })
         
 })
