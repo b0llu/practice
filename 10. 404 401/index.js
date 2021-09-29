32101c8a7f305bab5c5d4153d4847e65bef6b166
Dhruv <samantdhruv@gmail.com>
2021-09-29 15:26:26 +0530

    404 401 completed

--- 10. 404 401/index.js
+++ 10. 404 401/index.js
@@ -2,34 +2,30 @@ const error401Btn = document.querySelector("#error1-btn");
 const error404Btn = document.querySelector("#error4-btn");
 
 
-var serverURL1 = "https://unitube-server.herokuapp.com/playlists"
+var serverURL1 = "https://unitube-server.herokuapp.com/playlists/"
 var serverURL2 = "https://jsonplaceholder.typicode.com/dummyUsers"
 
-function getURL1(message) {
-    return serverURL1 + "?" + "text=" + message
-}
 
 error401Btn.addEventListener("click", function error401 () {
-    var inputError = "You Are Not Logged In"
-    fetch(getURL1(inputError))
-    .then(response => response.json())
-    .then(json => {
-        var errorMsg1 = json.message;
-        alert(errorMsg1)
-    })
-        
+    fetch(serverURL1)
+    .then(function(response){
+        if (response.status === 401) {
+            console.log(json)
+        }
+    }) 
+    .catch(function() {
+        alert("You Are Not Logged In")
+    } )
 })
 
-function getURL2(text) {
-    return serverURL2 + "?" + "text=" + text 
-}
-
-error404Btn.addEventListener("click", function error404 () {
-    var inputError = "Page Not Found"
-    fetch(getURL2(inputError))
-    .then(response => response.json())
-    .then(json => {
-        alert(json)
+error404Btn.addEventListener("click", function error404 (){
+    fetch(serverURL2)
+    .then(function(response){
+        if (response.status === 404) {
+            console.log(json)
+        }
     })
-        
+    .catch(function(){
+        alert("Page Not Found")
+    })
 })
