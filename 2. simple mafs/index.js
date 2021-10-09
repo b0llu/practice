ca21ac646c54cea7752628508d50eedac8d29d34
Dhruv <samantdhruv@gmail.com>
2021-10-09 09:59:21 +0530

    logic change in mafs

--- 2. simple mafs/index.js
+++ 2. simple mafs/index.js
@@ -5,31 +5,36 @@ const subBtn = document.querySelector("#sub-btn");
 const mulBtn = document.querySelector("#mul-btn");
 const divBtn = document.querySelector("#div-btn");
 let outputBox = document.querySelector("#output-box");
+
+function addition(numOne, numTwo) {
+    return Number(numOne) + Number(numTwo)
+}
  
+function subtraction(numOne, numTwo) {
+    return Number(numOne) - Number(numTwo)
+}
+
+function multiplication(numOne, numTwo) {
+    return Number(numOne) * Number(numTwo)
+}
+
+function division(numOne, numTwo) {
+    return Number(numOne) / Number(numTwo)
+}
+
+
 addBtn.addEventListener("click", ()=>{
-    let x = numOne.value;
-    let y = numTwo.value;
-    let z = Number(x) + Number(y);
-    outputBox.textContent = z;  
+    outputBox.textContent = addition(numOne.value, numTwo.value);  
 })
 
 subBtn.addEventListener("click", ()=>{
-    let x = numOne.value;
-    let y = numTwo.value;
-    let z = Number(x) - Number(y);
-    outputBox.textContent = z;  
+    outputBox.textContent = subtraction(numOne.value, numTwo.value);  
 })
 
 mulBtn.addEventListener("click", ()=>{
-    let x = numOne.value;
-    let y = numTwo.value;
-    let z = Number(x) * Number(y);
-    outputBox.textContent = z;  
+    outputBox.textContent = multiplication(numOne.value, numTwo.value);  
 })
 
 divBtn.addEventListener("click", ()=>{
-    let x = numOne.value;
-    let y = numTwo.value;
-    let z = Number(x) / Number(y);
-    outputBox.textContent = z;  
+    outputBox.textContent = division(numOne.value, numTwo.value);  
 })
