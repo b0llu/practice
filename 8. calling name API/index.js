8304053f9aed8e9842d538d9a51b94b614668b1a
Dhruv <samantdhruv@gmail.com>
2021-11-10 10:36:33 +0530

    logic change for json name change

--- 8. calling name API/index.js
+++ 8. calling name API/index.js
@@ -2,19 +2,19 @@ const inputText = document.querySelector("#input-text");
 const nextBtn = document.querySelector("#next-btn");
 const outputDiv = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-function getURL(text){
-    return serverURL + "?" + "text=" + text
+function getURL(text) {
+  return serverURL + "?" + "text=" + text;
 }
 
-nextBtn.addEventListener("click", function api () {
-    var inputTxt = inputText.value;
-    fetch(getURL(inputTxt))
-    .then(response => response.json())
-    .then(json => {
-        var contentsText = json.contents.text;
-        outputDiv.innerHTML = contentsText
-    })
-        
-})
+nextBtn.addEventListener("click", function api() {
+  var inputTxt = inputText.value;
+  fetch(getURL(inputTxt))
+    .then((response) => response.json())
+    .then((json) => {
+      outputDiv.innerHTML = `${
+        json.contents.translated
+      } ${json.contents.text.toUpperCase()}`;
+    });
+});
